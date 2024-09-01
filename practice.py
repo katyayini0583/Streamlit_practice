07da89d25c4bf4421ce8aee816ace69a7c272524
Katyayini Koneti <31547166+katyayini0583@users.noreply.github.com>
2024-09-01 17:19:54 +0530

    Update practice.py

--- practice.py
+++ practice.py
@@ -5,5 +5,6 @@ import streamlit as st
 st.header('MLOps Deployment Practice')
 name = st.text_input("May I know your name")
 
-st.button("Enter", st.write(f'Hello {name}.. Welcome to MLOps Practice'))
+if st.button("Enter"):
+    st.write(f'Hello {name}.. Welcome to MLOps Practice')
 
