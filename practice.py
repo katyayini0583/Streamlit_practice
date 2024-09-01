b1bd1c12e77e6375310fbb1525acfcf2806339ac
Katyayini Koneti <31547166+katyayini0583@users.noreply.github.com>
2024-09-01 18:36:12 +0530

    Update practice.py

--- practice.py
+++ practice.py
@@ -8,7 +8,7 @@ st.header('Stock Trends')
 
 symbols = ['MSFT', 'GOOGL', 'AMZN', 'TSLA']
 
-st.selectbox('Select the stock',symbols)
+stock_name = st.selectbox('Select the stock',symbols)
 col1,col2 = st.columns(2)
 start_date = col1.date_input("Choose start date", datetime.date(2019, 7, 6))
 end_date = col2.date_input("Choose end date", datetime.date(2019, 7, 6))
@@ -18,6 +18,6 @@ data = yf.download("AAPL",progress=False,start=start_date,end=end_date)
 #st.dataframe(data)
 #data['Close'].plot(Title='Apple stocks close chart')
 
-st.subheader(f'Apple stocks closing trend between {start_date} and {end_date}')
+st.subheader(f'{stock_name} stocks closing trend between {start_date} and {end_date}')
 if st.button("Get Chart"):
     st.line_chart(data['Close'])
