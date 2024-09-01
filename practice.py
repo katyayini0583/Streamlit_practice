104a56b5389e3be6f20c41d63d3b5bfe7fd4a069
Katyayini Koneti <31547166+katyayini0583@users.noreply.github.com>
2024-09-01 17:24:38 +0530

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
+import yfinance as yf
 
 st.header('MLOps Deployment Practice')
 name = st.text_input("May I know your name")
@@ -8,3 +9,5 @@ name = st.text_input("May I know your name")
 if st.button("Enter"):
     st.write(f'Hello {name}.. Welcome to MLOps Practice')
 
+data = yf.download("SPY AAPL", period="1mo")
+st.dataframe(data)
