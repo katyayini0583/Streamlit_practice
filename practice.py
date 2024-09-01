73301a07b53fb173751b3df0c85c4b4774864d91
Katyayini Koneti <31547166+katyayini0583@users.noreply.github.com>
2024-09-01 17:18:34 +0530

    Update practice.py

--- practice.py
+++ practice.py
@@ -6,7 +6,4 @@ st.header('MLOps Deployment Practice')
 name = st.text_input("May I know your name")
 
 st.button("Enter", type="primary")
-if st.button("Say hello"):
-    st.write(f'Hello {name}.. Welcome to MLOps Practice')
-else:
-    st.write("Goodbye")
+st.write(f'Hello {name}.. Welcome to MLOps Practice')
