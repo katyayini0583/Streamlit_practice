a18b6cc192b0a35f90ae23c7f86a9a8842e91ae1
Katyayini Koneti <31547166+katyayini0583@users.noreply.github.com>
2024-09-01 18:23:52 +0530

    Update practice.py

--- practice.py
+++ practice.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 import yfinance as yf
+import datetime
 
 st.header('MLOps Deployment Practice')
 name = st.text_input("May I know your name")
@@ -9,7 +10,10 @@ name = st.text_input("May I know your name")
 if st.button("Enter"):
     st.write(f'Hello {name}.. Welcome to MLOps Practice')
 
-data = yf.download("AAPL", period="1mo",progress=False)
+start_date = st.date_input("Choose start date", datetime.date(2019, 7, 6))
+end_date = st.date_input("Choose end date", datetime.date(2019, 7, 6))
+
+data = yf.download("AAPL",progress=False,start=start_date,end=end_date)
 #st.write(f'{data}')
 st.dataframe(data)
 #data['Close'].plot(Title='Apple stocks close chart')
