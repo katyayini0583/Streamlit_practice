d6925269c95161dd5f0d2f862ef253d5873ffe4c
Katyayini Koneti <31547166+katyayini0583@users.noreply.github.com>
2024-09-01 18:43:04 +0530

    Update practice.py

--- practice.py
+++ practice.py
@@ -11,7 +11,7 @@ symbols = ['NSE','MSFT', 'GOOGL', 'AMZN', 'TSLA']
 stock_name = st.selectbox('Select the stock',symbols)
 col1,col2 = st.columns(2)
 start_date = col1.date_input("Choose start date", datetime.date(2019, 7, 6))
-end_date = col2.date_input("Choose end date", datetime.date(2019, 7, 6))
+end_date = col2.date_input("Choose end date", datetime.date(2019, 8, 6))
 
 data = yf.download("AAPL",progress=False,start=start_date,end=end_date)
 #st.write(f'{data}')
