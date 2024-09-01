14083b01f2c78649810f3369f0dad750e4d0b5d1
Katyayini Koneti <31547166+katyayini0583@users.noreply.github.com>
2024-09-01 18:16:33 +0530

    Update practice.py

--- practice.py
+++ practice.py
@@ -10,21 +10,5 @@ if st.button("Enter"):
     st.write(f'Hello {name}.. Welcome to MLOps Practice')
 
 data = yf.download("AAPL", period="1mo",progress=False)
-st.write(f'{data}')
+#st.write(f'{data}')
 st.dataframe(data)
-'''
-#ticker = yf.Ticker('AAPL',progress=False)
-#aapl_df = ticker.history(period="1y")
-#aapl_df['Close'].plot(title="APPLE's stock price")
-
-
-tickers = yf.Tickers('msft aapl goog')
-
-# access each ticker using (example)
-tickers.tickers['MSFT'].info
-tickers.tickers['AAPL'].history(period="1mo")
-#df = tickers.tickers['AAPL'].history(period="1mo",progress=False)
-#df['close'].plot(title="APPLE's stock price")
-tickers.tickers['GOOG'].actions
-
-'''
