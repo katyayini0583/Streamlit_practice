6079f39f4e3dbfa6e7a04d6e73bb7ef2e7f60069
Katyayini Koneti <31547166+katyayini0583@users.noreply.github.com>
2024-09-01 17:16:05 +0530

    Update practice.py

--- practice.py
+++ practice.py
@@ -4,4 +4,4 @@ import streamlit as st
 
 st.header('MLOps Deployment Practice')
 name = st.text_input("May I know your name")
-st.write('Hello {name}.. Welcome to MLOps Practice')
+st.write(f'Hello {name}.. Welcome to MLOps Practice')
