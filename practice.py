19d6e612c0c8703eae866275907d39d516735091
Katyayini Koneti <31547166+katyayini0583@users.noreply.github.com>
2024-09-01 17:13:38 +0530

    new changes

--- practice.py
+++ practice.py
@@ -2,4 +2,6 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 
-st.header('Hello.. my new app')
+st.header('MLOps Deployment Practice')
+name = st.text_input("May I know your name")
+st.write('Hello {name}.. Welcome to MLOps Practice')
